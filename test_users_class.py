ad3a73edb24b3157c9175e057b449d2e32938c64
Dirac Murairi <diracm8@gmail.com>
2020-11-29 22:08:47 +0200

    add one test in users

--- test_users_class.py
+++ test_users_class.py
@@ -5,3 +5,18 @@
 from users_class import *
 import unittest
 from unittest import mock
+
+class TestSolution(unittest.TestCase):
+    @mock.patch("user.input", create=True)
+    def negative_test_find_specific_book(self, mocked_input):
+        user1 = User("DIRAC", "d.murairi@alustudent.com", 1)
+        mocked_input.side_effect = ["SWIFT"]
+        i = user1.find_specific_book(books)
+        self.assertEqual(i, 0)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
